81d381790ceeb35e938d0bb6d5240a57b46520ec
Gregory Patlasov <gregory.patlasov@gmail.com>
2026-02-18 18:48:03 +0300

    2-й коммит 2 задания

--- r2r_dac.py
+++ r2r_dac.py
@@ -1,58 +1,43 @@
 import RPi.GPIO as GPIO
 
 class R2R_DAC:
-    def __init__(self, gpio_bits, dynamic_range, verbose=False):
+    def __init__(self, gpio_bits, dynamic_range, verbose = False):
         self.gpio_bits = gpio_bits
         self.dynamic_range = dynamic_range
         self.verbose = verbose
 
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.gpio_bits, GPIO.OUT, initial=0)
-        # if self.verbose:
-        #     print(f"R2R_DAC инициализирован: пины {self.gpio_bits}, диапазон {self.dynamic_range} В")
+        GPIO.setup(self.gpio_bits, GPIO.OUT, initial = 0)
 
     def deinit(self):
-        GPIO.output(self.gpio_bits, 0)
+        GPIO.putput(self.gpio_bits, 0)
         GPIO.cleanup()
-        # if self.verbose:
-        #     print("R2R_DAC деинициализирован")
 
     def set_number(self, number):
-        if number < 0 or number > 255:
-            print(f"Ошибка: число {number} вне диапазона 0-255. Устанавливается 0.")
-            number = 0
-
-        for i, pin in enumerate(self.gpio_bits):
-            state = (number >> i) & 1
-            GPIO.output(pin, state)
-
-        # if self.verbose:
-        #     print(f"Установлено число {number} (0x{number:02X}) на ЦАП")
+        a = [int(i) for i in bin(number)[2:].zfill(8)]
+        print(a)
+        for i in range(len(a)):
+            GPIO.output(self.gpio_bits[i], a[i])
 
     def set_voltage(self, voltage):
         if not (0.0 <= voltage <= self.dynamic_range):
-            print(f"Напряжение {voltage:.3f} В выходит за диапазон 0.00 - {self.dynamic_range:.2f} В. Устанавливается 0 В.")
-            number = 0
-        else:
-            number = int(voltage / self.dynamic_range * 255)
-
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} В)")
+            print("Устанавливаем 0.0 В")
+            return 0
+        number = int(voltage / self.dynamic_range * 255)
         self.set_number(number)
 
-        # if self.verbose:
-        #     actual_voltage = number * self.dynamic_range / 255
-        #     print(f"Запрошено {voltage:.3f} В -> число {number} -> фактическое напряжение ~{actual_voltage:.3f} В")
-
-
-if name == "__main__":
+if __name__ == "__main__":
     try:
-        # Создаём объект ЦАП с пинами из задания (BCM нумерация)
-        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, verbose=True)
+        dac = R2R_DAC([16, 20, 21, 25, 26, 17, 27, 22], 3.183, True)
 
         while True:
             try:
-                voltage = float(input("Введите напряжение в Вольтах: "))
+                voltage = float(input("Введите напряжение в вольтах: "))
                 dac.set_voltage(voltage)
+
             except ValueError:
-                print("Вы ввели не число. Попробуйте ещё раз\n")
+                print("Вы ввели не число. Попробуйте еще раз\n")
+
     finally:
         dac.deinit()
